d7f79b7bda5e40da84aef2d688687d8850ece362
Aladeabass <aladeabass2004@gmail.com>
2022-09-19 14:55:20 +0100

    e-commerce

---
+++ js basics.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+console.error(200);
+console.warn("this is a warning");
+
+
+
+const name='alade';
+const age=30;
+
+console.log('My name is' +  name + 'and i am'+age)
+
